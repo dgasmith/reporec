00742a15a6bcebe26791345b56ca89d9db7972dc
Daniel Smith <malorian@me.com>
2019-03-18 21:13:13 -0400

    Fixes conda tests

--- reporec/tests/test_conda.py
+++ reporec/tests/test_conda.py
@@ -9,11 +9,11 @@ import reporec
 
 def test_conda_downloads():
 
-    ret = reporec.conda.get_downloads("dsmith", "qcengine")
+    ret = reporec.conda.get_downloads("conda-forge", "qcengine")
     assert len(ret) > 1
     assert ret[0].keys() == {"timestamp", "version", "downloads"}
 
 def test_conda_downloads_error():
 
     with pytest.raises(requests.exceptions.HTTPError):
-        ret = reporec.conda.get_downloads("dsmith", "cookiemonster")
+        ret = reporec.conda.get_downloads("molssi", "cookiemonster")
